9d44269f39068ffdf9b995bc8af8a67587f74ff2
Santos <clarasantosmf@gmail.com>
2024-01-17 10:36:40 -0300

    Um type

--- test/conftest.py
+++ test/conftest.py
@@ -6,7 +6,6 @@ from torneios import db, create_app
 def app():
     app = create_app("testing")
     with app.app_context():
-        db.create_all()
         yield app
         db.session.rollback()
         db.session.close()
