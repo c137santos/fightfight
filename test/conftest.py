2fcbfe08363f924902b592f4cb452096530f08a8
Santos <clarasantosmf@gmail.com>
2024-01-17 10:00:57 -0300

    Utilizando o DB de teste corretamente, mas ainda com problema de isolamento

--- test/conftest.py
+++ test/conftest.py
@@ -5,9 +5,11 @@ from torneios import db, create_app
 @pytest.fixture(scope="module")
 def app():
     app = create_app("testing")
-    with app.app_context():   
+    with app.app_context():
         db.create_all()
-        yield app 
+        yield app
+        db.session.rollback()
+        db.session.close()
         db.drop_all()
 
 
@@ -15,7 +17,3 @@ def app():
 def client(app):
     with app.test_client() as client:
         yield client
-
-@pytest.fixture()
-def db_session(app):
-    yield db
--- test/test_models.py
+++ test/test_models.py
@@ -1,13 +1,14 @@
 from torneios.models import Torneio, Competidor
+from torneios import db
 
 
-def test_new_tournament(db_session):
+def test_new_tournament(app):
     torneio = Torneio(nome_torneio="Primeiro torneio")
-    db_session.add(torneio)
-    db_session.commit()
-    torneio_adicionado = (
-        db_session.query(Torneio).filter_by(nome_torneio="Primeiro torneio").first()
-    )
+    db.session.add(torneio)
+    db.session.commit()
+    torneio_adicionado = Torneio.query.filter_by(
+        nome_torneio="Primeiro torneio"
+    ).first()
     assert torneio_adicionado is not None
     assert torneio_adicionado.nome_torneio == "Primeiro torneio"
     assert torneio_adicionado.qtd_competidores == 0
@@ -15,20 +16,17 @@ def test_new_tournament(db_session):
     assert torneio_adicionado.is_chaveado is False
 
 
-def test_new_competidor(db_session):
+def test_new_competidor(app):
     nome = "Competidor Super"
     torneio = Torneio(nome_torneio="Primeiro torneio")
-    db_session.add(torneio)
-    db_session.commit()
+    db.session.add(torneio)
+    db.session.commit()
 
     competidor = Competidor(nome_competidor=nome, torneio_id=torneio.id)
     competidor.torneio = torneio
-    db_session.add(competidor)
-    db_session.commit()
-    competidor_add = (
-        db_session.query(Competidor).filter_by(nome_competidor=nome).first()
-    )
-
+    db.session.add(competidor)
+    db.session.commit()
+    competidor_add = Competidor.query.filter_by(nome_competidor=nome).first()
     assert competidor_add.nome_competidor == nome
     assert competidor_add.id == competidor.id
     assert competidor_add.torneio_id == torneio.id
